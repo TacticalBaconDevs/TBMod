038d14e643193b58a5f1c1701fded359804a7aaf
Will <shukari94@gmail.com>
2018-08-10 14:23:40 +0200

    BuildingTools Anpassungen

--- #buildTools/pbo_alle.bat
+++ #buildTools/pbo_alle.bat
@@ -1,10 +1,10 @@
 SET armaFolder="E:\SteamLibrary\steamapps\common\Arma 3\#MissionMods\@TBMod_dev\addons"
 
 :: -BD statt -B; -P wenn keine Pause
-MakePbo.exe -A -B -D -P -X=*.txt,*.dep,*.cpp,*.bak,*.png,*.log,*.pew,*.ini,*.rar TBMod_arsenal %armaFolder%
-MakePbo.exe -A -B -D -P -X=*.txt,*.dep,*.cpp,*.bak,*.png,*.log,*.pew,*.ini,*.rar TBMod_main %armaFolder%
-MakePbo.exe -A -B -D -P -X=*.txt,*.dep,*.cpp,*.bak,*.png,*.log,*.pew,*.ini,*.rar TBMod_nachschub %armaFolder%
-::MakePbo.exe -A -B -D -P -X=*.txt,*.dep,*.cpp,*.bak,*.png,*.log,*.pew,*.ini,*.rar TBMod_ausbildung %armaFolder%
-MakePbo.exe -A -B -D -P -X=*.txt,*.dep,*.cpp,*.bak,*.png,*.log,*.pew,*.ini,*.rar TBMod_building %armaFolder%
-MakePbo.exe -A -B -D -P -X=*.txt,*.dep,*.cpp,*.bak,*.png,*.log,*.pew,*.ini,*.rar TBMod_items %armaFolder%
-MakePbo.exe -A -B -D -P -X=*.txt,*.dep,*.cpp,*.bak,*.png,*.log,*.pew,*.ini,*.rar TBMod_zusatzStuff %armaFolder%
+MakePbo.exe -A -B -D -P -X=*.txt,*.dep,*.cpp,*.bak,*.png,*.log,*.pew,*.ini,*.rar addons\TBMod_arsenal %armaFolder%
+MakePbo.exe -A -B -D -P -X=*.txt,*.dep,*.cpp,*.bak,*.png,*.log,*.pew,*.ini,*.rar addons\TBMod_main %armaFolder%
+MakePbo.exe -A -B -D -P -X=*.txt,*.dep,*.cpp,*.bak,*.png,*.log,*.pew,*.ini,*.rar addons\TBMod_nachschub %armaFolder%
+::MakePbo.exe -A -B -D -P -X=*.txt,*.dep,*.cpp,*.bak,*.png,*.log,*.pew,*.ini,*.rar addons\TBMod_ausbildung %armaFolder%
+MakePbo.exe -A -B -D -P -X=*.txt,*.dep,*.cpp,*.bak,*.png,*.log,*.pew,*.ini,*.rar addons\TBMod_building %armaFolder%
+MakePbo.exe -A -B -D -P -X=*.txt,*.dep,*.cpp,*.bak,*.png,*.log,*.pew,*.ini,*.rar addons\TBMod_items %armaFolder%
+MakePbo.exe -A -B -D -P -X=*.txt,*.dep,*.cpp,*.bak,*.png,*.log,*.pew,*.ini,*.rar addons\TBMod_zusatzStuff %armaFolder%
