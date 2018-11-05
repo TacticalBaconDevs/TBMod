13ee9813c39434a5251ed2726083e3b02e6a4a11
Eric van Fandenfart <ruhlanderic@gmail.com>
2018-11-05 20:07:03 +0100

    Wrong Stringrepresentation

---
+++ addons/#TBMod_rhs/CfgVehicles.hpp
@@ -0,0 +1,10 @@
+/*
+    Part of the TBMod ( https://github.com/shukari/TBMod )
+    Developed by http://tacticalbacon.de
+
+    Author: shukari
+*/
+class CfgVehicles
+{
+    
+};
---
+++ addons/#TBMod_unsung/config.cpp
@@ -0,0 +1,17 @@
+class CfgPatches
+{
+    class TBMod_unsung
+    {
+        name = "TBMod Unsung";
+        
+        requiredAddons[] = {
+            "TBMod_nachschub",
+            "uns_main"
+        };
+        addonRootClass = "TBMod_nachschub";
+    };
+};
+
+// Configs
+#include "CfgVehicles.hpp"
+#include "CfgEditorSubcategories.hpp"
