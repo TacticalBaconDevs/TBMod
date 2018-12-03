f6464db4a95b66616cfc64f01c4322ae777144fa
IDarky <42013222+IDarky@users.noreply.github.com>
2018-12-03 19:30:14 +0100

    ups2

---
+++ #buildingTools/pbo_einzeln.bat
@@ -0,0 +1,4 @@
+SET armaFolder="K:\SSD Steam\steamapps\common\Arma 3\!#Bacon\@TBMod_dev\addons"
+
+:: -BD statt -B; -P wenn keine Pause
+FOR /D %%G in ("addons\TBMod*") DO MakePbo.exe -A -B -D -X=*.txt,*.dep,*.cpp,*.bak,*.png,*.log,*.pew,*.ini,*.rar addons\%%~nxG %armaFolder%
