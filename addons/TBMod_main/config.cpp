eed8614a36c7beda770d769775265be5aa5440f4
shukari <shukari@users.noreply.github.com>
2018-12-14 03:46:49 +0100

    PREP next Version

--- addons/TBMod_main/config.cpp
+++ addons/TBMod_main/config.cpp
@@ -11,9 +11,9 @@ class CfgPatches
         author = "shukari";
         authorUrl = "tacticalbacon.de";
 
-        version = 1.2.2;
-        versionStr = "1.2.2";
-        versionAr[] = {1,2,2};
+        version = 1.2.3;
+        versionStr = "1.2.3";
+        versionAr[] = {1,2,3};
         versionDesc = "TBMod";
         versionAct = "";
 
