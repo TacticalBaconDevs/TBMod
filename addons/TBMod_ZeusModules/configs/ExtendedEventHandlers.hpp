007a62a7e0dbd654ad07fe98c2351ef5aa831e59
shukari <shukari@users.noreply.github.com>
2019-01-14 12:16:37 +0100

    Update ExtendedEventHandlers.hpp

--- addons/TBMod_ZeusModules/configs/ExtendedEventHandlers.hpp
+++ addons/TBMod_ZeusModules/configs/ExtendedEventHandlers.hpp
@@ -2,7 +2,6 @@
     Part of the TBMod ( https://github.com/shukari/TBMod )
     Developed by http://tacticalbacon.de
 */
-
 class Extended_PreInit_EventHandlers
 {
     class TBMod_ZeusModules
@@ -10,5 +9,3 @@ class Extended_PreInit_EventHandlers
         init = "call compile preprocessFileLineNumbers '\TBMod_ZeusModules\XEH_preInit.sqf'";
     };
 };
-
-
