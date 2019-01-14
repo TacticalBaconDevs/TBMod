7d87b43056c5103227ab3bbd5d3c35cebcc622fc
shukari <shukari@users.noreply.github.com>
2019-01-14 12:16:13 +0100

    Update CfgVehicles.hpp

--- addons/TBMod_ZeusModules/configs/CfgVehicles.hpp
+++ addons/TBMod_ZeusModules/configs/CfgVehicles.hpp
@@ -2,7 +2,6 @@
     Part of the TBMod ( https://github.com/shukari/TBMod )
     Developed by http://tacticalbacon.de
 */
-
 class CfgVehicles
 {
     // ###################### Zeus Module ######################
@@ -38,12 +37,14 @@ class CfgVehicles
         author = "shukari";
         category = "TB_categorie";
     };
+    
     class TB_zeus_nachschubAbwurf : TB_zeus_base
     {
         scopeCurator = 2;
         displayName = "NachschubAbwurf";
         function = "TB_fnc_moduleNachschubAbwurf";
     };
+    
     class TB_zeus_disablePath : TB_zeus_base
     {
         scopeCurator = 2;
@@ -51,12 +52,14 @@ class CfgVehicles
         displayName = "DisablePath 4 Group";
         function = "TB_fnc_moduleDisablePath";
     };
+    
     class TB_zeus_prioSichtbarkeit : TB_zeus_base
     {
         scopeCurator = 2;
         displayName = "PrioSichtbarkeit";
         function = "TB_fnc_modulePrioSichtbarkeit";
     };
+    
     class TB_zeus_handleAsPlayerMedical : TB_zeus_base
     {
         scopeCurator = 2;
@@ -64,6 +67,7 @@ class CfgVehicles
         displayName = "HandlePlayerAsMedical";
         function = "TB_fnc_moduleHandleAsPlayerMedical";
     };
+    
     class TB_zeus_applyLoadout : TB_zeus_base
     {
         curatorCanAttach = 1;
