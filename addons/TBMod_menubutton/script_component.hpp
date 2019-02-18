a5c58683cddd890d62d612f5be972b12fa093dc0
IDarky <42013222+IDarky@users.noreply.github.com>
2019-02-18 17:42:18 +0100

    mainmenuButton

---
+++ addons/TBMod_menubutton/script_component.hpp
@@ -0,0 +1,17 @@
+#define COMPONENT button
+#define COMPONENT_BEAUTIFIED Button
+#include "\x\TBMod\addons\TBMod_main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+
+#ifdef DEBUG_ENABLED_BUTTON
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_BUTTON
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_BUTTON
+#endif
+
+#include "\x\TBMod\addons\TBMod_main\script_macros.hpp"
