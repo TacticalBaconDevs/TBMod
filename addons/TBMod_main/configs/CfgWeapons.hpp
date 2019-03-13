07e8a2355d0e4110922879f79d24e9bbd10b09c1
shukari <shukari@users.noreply.github.com>
2019-03-13 16:29:55 +0100

    Tabs to 4Leers

--- addons/TBMod_main/configs/CfgWeapons.hpp
+++ addons/TBMod_main/configs/CfgWeapons.hpp
@@ -7,7 +7,7 @@ class CfgWeapons {
     class ACE_atropine: ACE_ItemCore {
         scope = 2;
 
-		displayName = "Ketamin-Autoinjektor";
+        displayName = "Ketamin-Autoinjektor";
 
         descriptionShort = "Wird verwendet um leichte Schmerzen zu lindern.";
         descriptionUse = "Ein Schmerzmittel um leichte Schmerzen zu behandeln.";
