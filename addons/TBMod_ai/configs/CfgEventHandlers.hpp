528b8387fe28f5af173939f5aef5eea4b5d594f7
Eric van Fandenfart <ruhlanderic@gmail.com>
2019-03-16 13:18:52 +0100

    Update CfgEventHandlers.hpp

--- addons/TBMod_ai/configs/CfgEventHandlers.hpp
+++ addons/TBMod_ai/configs/CfgEventHandlers.hpp
@@ -4,6 +4,10 @@
 */
 class Extended_InitPost_EventHandlers
 {
-    class Wheeled_APC_F {init = "(_this select 0) allowCrewInImmobile true";};
-    class Tank {init = "(_this select 0) allowCrewInImmobile true";};
+    class Wheeled_APC_F {
+        init = "(_this select 0) allowCrewInImmobile true";
+    };
+    class Tank {
+        init = "(_this select 0) allowCrewInImmobile true";
+    };
 };
