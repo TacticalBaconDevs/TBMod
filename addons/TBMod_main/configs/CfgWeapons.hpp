00586148b5c3743577486f00ff8ef6a64b049191
shukari <shukari@users.noreply.github.com>
2019-03-18 13:02:06 +0100

    Update CfgWeapons.hpp

--- addons/TBMod_main/configs/CfgWeapons.hpp
+++ addons/TBMod_main/configs/CfgWeapons.hpp
@@ -2,11 +2,18 @@
     Part of the TBMod ( https://github.com/TacticalBaconDevs/TBMod )
     Developed by http://tacticalbacon.de
 */
-#define SET_NAME(CLASSE,DISPNAME) class CLASSE: ACE_ItemCore {displayName = DISPNAME;};
+#define SET_NAME(CLASSE,DISPNAME,GEWICHT) class CLASSE : ACE_ItemCore { \
+        displayName = DISPNAME; \
+        class ItemInfo: CBA_MiscItem_ItemInfo { \
+            mass = GEWICHT; \
+        }; \
+    }
 
 class CfgWeapons
 {
     class ACE_ItemCore;
+    class CBA_MiscItem_ItemInfo;
+    
     class ACE_atropine: ACE_ItemCore
     {
         displayName = "Ketamin-Autoinjektor";
@@ -14,15 +21,15 @@ class CfgWeapons
         descriptionUse = "Ein Schmerzmittel um leichte Schmerzen zu behandeln.";
     };
     
-    SET_NAME(ACE_bloodIV, "Blut IV (2000ml)");
-    SET_NAME(ACE_bloodIV_500, "Blut IV (1000ml)");
-    SET_NAME(ACE_bloodIV_250, "Blut IV (500ml)");
+    SET_NAME(ACE_bloodIV, "Blut IV (2000ml)", 10);
+    SET_NAME(ACE_bloodIV_500, "Blut IV (1000ml)", 5);
+    SET_NAME(ACE_bloodIV_250, "Blut IV (500ml)", 2.5);
     
-    SET_NAME(ACE_plasmaIV, "Plasma IV (1000ml)");
-    SET_NAME(ACE_plasmaIV_500, "Plasma IV (500ml)");
-    SET_NAME(ACE_plasmaIV_250, "Plasma IV (250ml)");
+    SET_NAME(ACE_plasmaIV, "Plasma IV (1000ml)", 20);
+    SET_NAME(ACE_plasmaIV_500, "Plasma IV (500ml)", 10);
+    SET_NAME(ACE_plasmaIV_250, "Plasma IV (250ml)", 5);
     
-    SET_NAME(ACE_salineIV, "Kochsalzlösung (500ml)");
-    SET_NAME(ACE_salineIV_500, "Kochsalzlösung (250ml)");
-    SET_NAME(ACE_salineIV_250, "Kochsalzlösung (125ml)");
+    SET_NAME(ACE_salineIV, "Kochsalzlösung (500ml)", 30);
+    SET_NAME(ACE_salineIV_500, "Kochsalzlösung (250ml)", 15);
+    SET_NAME(ACE_salineIV_250, "Kochsalzlösung (125ml)", 7.5);
 };
