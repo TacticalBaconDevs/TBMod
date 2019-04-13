37f5a7f8fc804281b767828ae20333253439d652
shukari <shukari@users.noreply.github.com>
2019-04-13 17:09:45 +0200

    v1.3.1

--- addons/TBMod_main/config.cpp
+++ addons/TBMod_main/config.cpp
@@ -14,9 +14,9 @@ class CfgPatches
         authorUrl = "https://github.com/TacticalBaconDevs/TBMod";
         license = "https://github.com/TacticalBaconDevs/TBMod/blob/master/LICENSE";
 
-        version = 1.3.0;
-        versionStr = "1.3.0";
-        versionAr[] = {1,3,0};
+        version = 1.3.1;
+        versionStr = "1.3.1";
+        versionAr[] = {1,3,1};
         versionDesc = "TBMod";
         versionAct = "";
 
