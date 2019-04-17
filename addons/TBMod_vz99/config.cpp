8039dcdd87e6e71b54ecd15a608fe41344fff6fe
shukari <shukari@users.noreply.github.com>
2019-04-18 01:38:04 +0200

    TaBs

--- addons/TBMod_vz99/config.cpp
+++ addons/TBMod_vz99/config.cpp
@@ -5,42 +5,42 @@
 */
 class CfgPatches
 {
-	class TBMod_vz99
-	{
-		name = "TBMod vz99";
+    class TBMod_vz99
+    {
+        name = "TBMod vz99";
 
-		author = "Potato";
-		authors[] = {"PabstMirror"};
-		authorUrl = "https://github.com/BourbonWarfare/POTATO";
+        author = "Potato";
+        authors[] = {"PabstMirror"};
+        authorUrl = "https://github.com/BourbonWarfare/POTATO";
 
         requiredAddons[] = {"TBMod_main"};
         addonRootClass = "TBMod_main";
 
-		units[] = {"TB_vz99_mortar", "TB_vz99_box"};
-		weapons[] = {"TB_vz99_carryWeapon"};
-		magazines[] = {
-			"TB_vz99_HE",
-			"TB_vz99_HE_multi",
-			"TB_vz99_HE_multi_charge0",
-			"TB_vz99_HE_PRX",
-			"TB_vz99_HE_PRX_charge0",
-			"TB_vz99_smokeWhite",
-			"TB_vz99_smokeWhite_charge0",
-			"TB_vz99_smokeRed",
-			"TB_vz99_smokeRed_charge0",
-			"TB_vz99_flare",
-			"TB_vz99_flare_charge0"
-		};
-		ammo[] = {
-			"TB_vz99_deploy_smokeWhite",
-			"TB_vz99_deploy_smokeRed",
-			"TB_vz99_ammo_smokeWhite",
-			"TB_vz99_ammo_smokeRed",
-			"TB_vz99_ammo_flare",
-			"TB_vz99_ammo_he",
-			"TB_vz99_ammo_he_airburst"
-		};
-	};
+        units[] = {"TB_vz99_mortar", "TB_vz99_box"};
+        weapons[] = {"TB_vz99_carryWeapon"};
+        magazines[] = {
+            "TB_vz99_HE",
+            "TB_vz99_HE_multi",
+            "TB_vz99_HE_multi_charge0",
+            "TB_vz99_HE_PRX",
+            "TB_vz99_HE_PRX_charge0",
+            "TB_vz99_smokeWhite",
+            "TB_vz99_smokeWhite_charge0",
+            "TB_vz99_smokeRed",
+            "TB_vz99_smokeRed_charge0",
+            "TB_vz99_flare",
+            "TB_vz99_flare_charge0"
+        };
+        ammo[] = {
+            "TB_vz99_deploy_smokeWhite",
+            "TB_vz99_deploy_smokeRed",
+            "TB_vz99_ammo_smokeWhite",
+            "TB_vz99_ammo_smokeRed",
+            "TB_vz99_ammo_flare",
+            "TB_vz99_ammo_he",
+            "TB_vz99_ammo_he_airburst"
+        };
+    };
 };
 
 // Configs
