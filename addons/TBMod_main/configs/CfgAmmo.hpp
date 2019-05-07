43e1efbf029a1fa33de9ea5adf04a9144bffc2cf
E-for-Eron <42243988+E-for-Eron@users.noreply.github.com>
2019-05-07 17:38:20 +0200

    Grenadier, Granaten, Gewichts/Inventaränderungen (#218)
    * Grenadier+Granaten+Gewichts/Inventaränderungen
    
    - alle Rauchgranten entfernt bis auf AIM
    - "Sticky" Granate für Grenadier, ULG Slug mit Türöffnungsscript, neue IR/Laser Granaten
    -
    
    * Handgranate implementiert
    
    * IR Handgranate komplett entfernt
    
    * Korrektur des DefaultLayouts aufgrund zu alter eigener Version
    
    * Fix zu Edit 1 und Edit 2
    
    * Vanilla Stuff nach Main; Referencen der Originalwerte; Optimierungen; Flash Stuff
    
    * TuBs
    
    * FiXeS
    
    * FlashFix
    
    * HET Effekt reingeklatscht
    
    * TtTTTTTaAaAAABbBbBbBsSSssSsSSSS

--- addons/TBMod_main/configs/CfgAmmo.hpp
+++ addons/TBMod_main/configs/CfgAmmo.hpp
@@ -4,6 +4,35 @@
 */
 class CfgAmmo
 {
+    // ###################### Unterlauf-IR Granate ######################
+    class B_IRStrobe;
+    class TB_rhs_40mm_IR_Grenade : B_IRStrobe
+    {
+        cost = 0; // 5000
+        dangerRadiusHit = 0; // -1
+        deflecting = 0; // 30
+        hit = 0; // 5
+        indirectHit = 0; // 2
+        indirectHitRange = 0; // 0.2
+        model = "\A3\Weapons_f\Data\bullettracer\tracer_yellow"; // \A3\Weapons_F_EPB\Ammo\B_IRstrobe_F.p3d
+        timeToLive = 900; // 300
+        tracerColor[] = {0.9,0.9,0.1,1}; // {0.7,0.7,0.5,0.04};
+        tracerColorR[] = {0.9,0.9,0.1,1}; // {0.7,0.7,0.5,0.04};
+        visibleFire = 0; // 2
+
+        // in Reference und Original nicht vorhanden - https://community.bistudio.com/wiki/Arma_3_Targeting_config_reference
+        visualTarget = 1;
+        visualTargetSize = 2;
+        irTarget = 1;
+        irTargetSize = 2;
+        laserTarget = 1;
+
+        // in Reference und Original nicht vorhanden - https://community.bistudio.com/wiki/Weapons_settings
+        tracerEndTime = 2;
+        tracerScale= 1.2;
+        tracerStartTime = 0.04;
+    };
+
     // ###################### FlareOverride ######################
     class FlareCore;
     class FlareBase: FlareCore
--- addons/TBMod_rhs/CfgMagazines.hpp
+++ addons/TBMod_rhs/CfgMagazines.hpp
@@ -14,7 +14,7 @@ class CfgMagazines
         displayName = "M713A Red Smoke Round";
         displaynameshort = "Red AIM Smoke";
 
-        mass = 15; //5
+        mass = 5; //5
     };
 
     class rhs_mag_m714_White;
@@ -27,7 +27,7 @@ class CfgMagazines
         displayName = "M714A White Smoke Round";
         displaynameshort = "White AIM Smoke";
 
-        mass = 15; //5
+        mass = 5; //5
     };
 
     class rhs_mag_m715_Green;
@@ -40,7 +40,7 @@ class CfgMagazines
         displayName = "M715A Green Smoke Round";
         displaynameshort = "Green AIM Smoke";
 
-        mass = 15; //5
+        mass = 5; //5
     };
 
     class rhs_mag_m716_Yellow;
@@ -53,12 +53,106 @@ class CfgMagazines
         displayName = "M716A Yellow Smoke Round";
         displaynameshort = "Yellow AIM Smoke";
 
-        mass = 15; //5
+        mass = 5; //5
     };
 
     class CA_LauncherMagazine;
     class rhs_fgm148_magazine_AT : CA_LauncherMagazine
     {
         mass = 176.37; // 286.88
+    };
+
+    class rhs_mag_M433_HEDP;
+    class TB_rhs_mag_40mm_HEDP : rhs_mag_M433_HEDP // Umbenennung zu TB_HEDP
+    {
+        ammo = "TB_rhs_40mm_HEDP";
+        author = "TBMod";
+
+        descriptionShort = "Type: TB HEDP Grenade Round<br />Caliber: 40 mm<br />Rounds: 1<br />Used in: M203, M320, M79";
+        displayName = "TB 40mm HEDP Grenade Round";
+        displaynameshort = "TB HEDP Grenade";
+
+        mass = 5; // 5.06
+    };
+
+    class rhs_mag_M441_HE;
+    class TB_rhs_mag_40mm_HE : rhs_mag_M441_HE // Umbenennung zu TB_HE
+    {
+        ammo = "TB_rhsusf_40mm_HET";
+        author = "TBMod";
+
+        descriptionShort = "Type: TB HET Grenade Round<br />Caliber: 40 mm<br />Rounds: 1<br />Used in: M203, M320, M79";
+        displayName = "TB 40mm HET Grenade Round";
+        displaynameshort = "TB HET Grenade";
+
+        mass = 5; // 5.06
+    };
+    class TB_rhs_mag_40mm_Delay : rhs_mag_M441_HE // Delay Grenade
+    {
+        ammo = "TB_rhs_40mm_Delay";
+        author = "TBMod";
+
+        descriptionShort = "Type: TB Delay Direct Grenade Round<br />Caliber: 40 mm<br />Rounds: 1<br />Used in: M203, M320, M79";
+        displayName = "TB 40mm Delay Direct Grenade Round";
+        displaynameshort = "TB Delay D Grenade";
+
+        initSpeed = 425; // 80
+        mass = 5; // 5.06
+        picture = "\rhsusf\addons\rhsusf_weapons2\icons\rhs_icon_m781_ca.paa"; // \rhsusf\addons\rhsusf_weapons2\icons\rhs_icon_m443_ca.paa
+        tracersEvery = 1; // 0
+    };
+
+    class rhsusf_8Rnd_Slug;
+    class TB_rhs_mag_40mm_Slugshot : rhsusf_8Rnd_Slug // Umbenennung zu TB_Slugshot
+    {
+        ammo = "TB_rhs_ammo_40mm_Slugshot";
+        author = "TBMod";
+
+        descriptionShort = "Type: TB Slugshot Grenade Round<br />Caliber: 40 mm<br />Rounds: 1<br />Used in: M203, M320, M79";
+        displayName = "TB 40mm Slugshot Grenade Round";
+        displaynameshort = "TB Slugshot Grenade";
+
+        count = 1; // 8
+        mass = 5; // 5.06
+        picture = "\rhsusf\addons\rhsusf_weapons\icons\m576_buckshot_ca.paa";
+    };
+
+    class rhs_mag_M585_white;
+    class TB_rhs_mag_M585_white : rhs_mag_M585_white // White Flare
+    {
+        ammo = "TB_rhsusf_40mm_white";
+        author = "TBMod";
+
+        descriptionShort = "Type: TB White Flare Grenade Round<br />Caliber: 40 mm<br />Rounds: 1<br />Used in: M203, M320, M79";
+        displayName = "TB 40mm White Flare Grenade Round";
+        displaynameshort = "TB White Flare Grenade";
+
+        mass = 3; // 5.06
+    };
+
+    class rhs_mag_m661_green;
+    class TB_rhs_mag_m661_green : rhs_mag_m661_green // Green Flare
+    {
+        ammo = "TB_rhsusf_40mm_green";
+        author = "TBMod";
+
+        descriptionShort = "Type: TB Green Flare Grenade Round<br />Caliber: 40 mm<br />Rounds: 1<br />Used in: M203, M320, M79";
+        displayName = "TB 40mm Green Flare Grenade Round";
+        displaynameshort = "TB Green Flare Grenade";
+
+        mass = 3; // 5.06
+    };
+
+    class rhs_mag_m662_red;
+    class TB_rhs_mag_m662_red : rhs_mag_m662_red // Red Flare
+    {
+        ammo = "TB_rhsusf_40mm_red";
+        author = "TBMod";
+
+        descriptionShort = "Type: TB Red Flare Grenade Round<br />Caliber: 40 mm<br />Rounds: 1<br />Used in: M203, M320, M79";
+        displayName = "TB 40mm Red Flare Grenade Round";
+        displaynameshort = "TB Red Flare Grenade";
+
+        mass = 3; // 5.06
     };
 };
