465500c7c6df95cf1c35797520faad346ba66e43
E-for-Eron <42243988+E-for-Eron@users.noreply.github.com>
2019-06-08 18:53:33 +0200

    Sniper Munition für Samstag (#244)
    * erste fertige Sniper Munition
    
    von alten PR entfernt, um für Sa hinzugefügt werden zu können
    
    * Abhängikeiten
    
    Abhängikeiten durch das heraustrennen aus alter PR vergessen

--- addons/TBMod_rhs/CfgAmmo.hpp
+++ addons/TBMod_rhs/CfgAmmo.hpp
@@ -155,4 +155,13 @@ class CfgAmmo
     {
         submunitionAmmo = "APERSBoundingMine_Range_Ammo"; // "APERSMine_Range_Ammo"
     };
+
+    class rhsusf_ammo_127x99_M33_Ball;
+    class rhsusf_ammo_127x99_mk211 : rhsusf_ammo_127x99_M33_Ball // 50.cal HEIAP-T
+    {
+        caliber = 10.3; // 2.05761
+        hit = 39.7; // 24.8085
+        indirectHitRange = 1.66; // 2.76
+        tracerEndTime = 4.5; // 3
+    };
 };
