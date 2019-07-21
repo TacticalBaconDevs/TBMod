8ebe3713df4785523b7e47c5324081af16581564
IDarky <42013222+IDarky@users.noreply.github.com>
2019-07-21 22:39:57 +0200

    Update CfgVehicles_BW.hpp

--- addons/TBMod_nachschub/configs/CfgVehicles_BW.hpp
+++ addons/TBMod_nachschub/configs/CfgVehicles_BW.hpp
@@ -8,10 +8,10 @@ class TB_supply_bw_ammo : WRAPPER_NAME(Box_IND_Wps_F)
 
     class TransportMagazines
     {
-        MACRO_ADDMAGAZINE(BWA3_200Rnd_556x45_Tracer,2);
-        MACRO_ADDMAGAZINE(BWA3_10Rnd_762x51_G28,4);        //G27 Munition
-        MACRO_ADDMAGAZINE(BWA3_20Rnd_762x51_G28,4);
-        MACRO_ADDMAGAZINE(BWA3_30Rnd_556x45_G36_Tracer,10);
+        MACRO_ADDMAGAZINE(BWA3_200Rnd_556x45_Tracer,4);
+        MACRO_ADDMAGAZINE(BWA3_10Rnd_762x51_G28,5);        //G27 Munition
+        MACRO_ADDMAGAZINE(BWA3_20Rnd_762x51_G28,5);
+        MACRO_ADDMAGAZINE(BWA3_30Rnd_556x45_G36_Tracer,15);
         MACRO_ADDMAGAZINE(BWA3_30Rnd_556x45_G36,15);    
     };
 };
