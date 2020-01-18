0cbca17fd136e87566aa0289b51849839033f252
Eric van Fandenfart <ruhlanderic@gmail.com>
2020-01-18 22:59:40 +0100

    Tasks (#401)
    * Update fnc_persistenceBuilding.sqf
    
    * Persistence Building zu Persistence
    
    * Tasks Init
    
    * Loop & Renames
    
    * Clean up
    
    * tabs
    
    * include to much
    
    * more renames
    
    * Fixes
    
    * Persistence Tasks
    
    * ta   bs
    
    * Update addons/persistence/functions/fnc_persistence.sqf
    
    Co-Authored-By: shukari <shukari@users.noreply.github.com>
    
    * Update addons/persistence/functions/fnc_persistence.sqf
    
    Co-Authored-By: shukari <shukari@users.noreply.github.com>
    
    * Update addons/tasks/XEH_postServerInit.sqf
    
    Co-Authored-By: shukari <shukari@users.noreply.github.com>
    
    * Update fnc_loadTask.sqf
    
    * Update ReadMe.md
    
    * Fixes & GlobalState
    
    Co-authored-by: shukari <shukari@users.noreply.github.com>

---
+++ addons/tasks/script_component.hpp
@@ -0,0 +1,15 @@
+/*
+    Part of the TBMod ( https://github.com/TacticalBaconDevs/TBMod )
+    Developed by http://tacticalbacon.de
+*/
+#define COMPONENT tasks
+#include "\x\TBMod\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+
+#include "\x\TBMod\addons\main\script_macros.hpp"
+#define STATE_NotInitialised 0
+#define STATE_Initialised 1
+#define STATE_Completed 2
+#define STATE_Failed 3
