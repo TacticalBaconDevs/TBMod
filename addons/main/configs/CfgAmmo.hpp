ee9a577e53700ecfa27633d8e460a14dcc3c0890
shukari <shukari@users.noreply.github.com>
2020-01-27 10:55:24 +0100

    T     A     B     S

--- addons/main/configs/CfgAmmo.hpp
+++ addons/main/configs/CfgAmmo.hpp
@@ -93,7 +93,7 @@ class CfgAmmo
     // ADD_PRECISE_SMOKE(G_40mm_SmokeRed);
     // ADD_PRECISE_SMOKE(G_40mm_SmokeYellow);
 
-	// Smoke für Fahrzeuge, benötigt ClassEventhandler aus PreInit
+    // Smoke für Fahrzeuge, benötigt ClassEventhandler aus PreInit
     class TB_G_40mm_Smoke : G_40mm_Smoke
     {
         simulation = "shotShell";
