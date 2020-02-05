6811af4be42a53efaddfe5201df89bc43b3cdfe3
shukari <shukari@users.noreply.github.com>
2020-02-05 15:40:32 +0100

    TuBsIs

--- addons/main/configs/TB_RscHTML.hpp
+++ addons/main/configs/TB_RscHTML.hpp
@@ -15,33 +15,33 @@
 class RscHTML;
 class RscText;
 class TB_RscHTML {
-	idd = 70500;
+   idd = 70500;
     onLoad = "uiNamespace setVariable ['TB_RscHTML', _this # 0]; ((_this # 0) displayCtrl 1800) htmlLoad (uiNamespace getVariable ['TB_RscHTML_url', '']);";
     onKeyDown = "(_this # 0) closeDisplay 1";
 
-	/*class controlsBackground
+   /*class controlsBackground
     {
         class TB_MainBackground : RscText
         {
-			idc = -1;
-			x = 0.29375 * safezoneW + safezoneX;
-			y = 0.225 * safezoneH + safezoneY;
-			w = 0.4125 * safezoneW;
-			h = 0.55 * safezoneH;
+         idc = -1;
+         x = 0.29375 * safezoneW + safezoneX;
+         y = 0.225 * safezoneH + safezoneY;
+         w = 0.4125 * safezoneW;
+         h = 0.55 * safezoneH;
             colorBackground[] = {0, 0, 0, 0.7};
-		};
+      };
     };*/
 
-	class controls
+   class controls
     {
-		class TB_RscHTML : RscHTML
-		{
-			idc = 1800;
-			x = 0.29375 * safezoneW + safezoneX;
-			y = 0.225 * safezoneH + safezoneY;
-			w = 0.4125 * safezoneW;
-			h = 0.55 * safezoneH;
+      class TB_RscHTML : RscHTML
+      {
+         idc = 1800;
+         x = 0.29375 * safezoneW + safezoneX;
+         y = 0.225 * safezoneH + safezoneY;
+         w = 0.4125 * safezoneW;
+         h = 0.55 * safezoneH;
             colorBackground[] = {0, 0, 0, 0.7};
-		};
-	};
+      };
+   };
 };
