017f747808d87b1b533bd3c3d5d26999a772db9d
shukari <shukari@users.noreply.github.com>
2020-02-26 08:56:48 +0100

    Update CfgVehicles.hpp

--- addons/eden/configs/CfgVehicles.hpp
+++ addons/eden/configs/CfgVehicles.hpp
@@ -421,7 +421,7 @@ class CfgVehicles
                 displayName = "Wundort";
                 tooltip = "Wo die Quelle PRIMÄR wirken soll, es geht trotzdem auch auf benachbarte Orte";
                 typeName = "STRING";
-                defaultValue = """['head', 'torso', 'hand_r', 'hand_l', 'leg_r', 'leg_l']""";
+                defaultValue = """['head', 'body', 'hand_l', 'hand_r', 'leg_l', 'leg_r']""";
             };
 
             class ModuleDescription: ModuleDescription {};
