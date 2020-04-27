2bfa533f7d79cf2d23374e2c16ab86de8ccf6f70
shukari <shukari@users.noreply.github.com>
2020-04-27 11:56:43 +0200

    tAbS

--- addons/main/gcam/gcam_component.hpp
+++ addons/main/gcam/gcam_component.hpp
@@ -406,9 +406,9 @@ class GCam_DialogBase_ListBox
         colorActive[] = {1, 1, 1, 1};
         colorDisabled[] = {1, 1, 1, 0.3};
         thumb="\A3\ui_f\data\gui\cfg\scrollbar\thumb_ca.paa";
-	    arrowEmpty="\A3\ui_f\data\gui\cfg\scrollbar\arrowEmpty_ca.paa";
-	    arrowFull="\A3\ui_f\data\gui\cfg\scrollbar\arrowFull_ca.paa";
-	    border="\A3\ui_f\data\gui\cfg\scrollbar\border_ca.paa";
+        arrowEmpty="\A3\ui_f\data\gui\cfg\scrollbar\arrowEmpty_ca.paa";
+        arrowFull="\A3\ui_f\data\gui\cfg\scrollbar\arrowFull_ca.paa";
+        border="\A3\ui_f\data\gui\cfg\scrollbar\border_ca.paa";
     };
 
     class ListScrollBar
