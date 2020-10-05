51de557989241165dfa423557ecdeb1164109e75
Eric van Fandenfart <ruhlanderic@gmail.com>
2020-10-05 19:29:18 +0200

    Safestart aktivieren wenn Mission beendet wird (#530)
    * Create fnc_moduleEndMission.sqf
    
    * Tabs
    
    * move to Zeus mod

---
+++ addons/zeus/configs/CfgFunctions.hpp
@@ -0,0 +1,18 @@
+/*
+    Part of the TBMod ( https://github.com/TacticalBaconDevs/TBMod )
+    Developed by http://tacticalbacon.de
+*/
+class CfgFunctions
+{
+    // REPLACEMENT - BIS_fnc_moduleEndMission
+    class A3_Modules_F_Curator
+    {
+        class Intel
+        {
+            class moduleEndMission
+            {
+                file = QPATHTOF(replacement\fnc_moduleEndMission.sqf);
+            };
+        };
+    };
+};
