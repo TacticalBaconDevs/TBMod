481186918d2e00d5e77fdd9627a04472f21e21cf
E-for-Eron <42243988+E-for-Eron@users.noreply.github.com>
2021-01-16 17:12:52 +0100

    Glock18 (#594)
    * Glock18
    
    * Tabs durch Leerzeichen ausgetauscht
    
    Warum ist das notwendig? Sollte in VSC nicht automatisch jedes Tab aus vier Leerzeichen bestehen?
    
    * G17 wieder raus, war nur als Vergleich gedacht

---
+++ addons/rhs/CfgRecoils.hpp
@@ -0,0 +1,14 @@
+/*
+    Part of the TBMod ( https://github.com/TacticalBaconDevs/TBMod )
+    Developed by http://tacticalbacon.de
+*/
+class CfgRecoils
+{
+    class TB_recoil_glock18
+    {
+        kickBack[] = {0.04,0.08};
+        muzzleOuter[] = {0.05,0.45,0.2,0.2};
+        permanent = 0.01;
+        temporary = 0.005;
+    };
+};
