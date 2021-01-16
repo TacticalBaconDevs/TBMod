41d1864d282be6eb6402e7c30393ebc1fe69080a
E-for-Eron <17111@gmx.de>
2021-01-16 02:09:57 +0100

    Tabs durch Leerzeichen ausgetauscht
    Warum ist das notwendig? Sollte in VSC nicht automatisch jedes Tab aus vier Leerzeichen bestehen?

--- addons/rhs/CfgRecoils.hpp
+++ addons/rhs/CfgRecoils.hpp
@@ -4,18 +4,18 @@
 */
 class CfgRecoils
 {
-	class TB_recoil_glock17
-	{
-		kickBack[] = {"0.03*1.4","0.06*1.4"};
-		muzzleOuter[] = {"0.2*0.2","1*1","0.2*1","0.3*1"};
-		permanent = "0.1*0.4";
-		temporary = "0.03*1.2";
-	};
-	class TB_recoil_glock18
-	{
-		kickBack[] = {0.04,0.08};
-		muzzleOuter[] = {0.05,0.45,0.2,0.2};
-		permanent = 0.01;
-		temporary = 0.005;
-	};
+    class TB_recoil_glock17
+    {
+        kickBack[] = {"0.03*1.4","0.06*1.4"};
+        muzzleOuter[] = {"0.2*0.2","1*1","0.2*1","0.3*1"};
+        permanent = "0.1*0.4";
+        temporary = "0.03*1.2";
+    };
+    class TB_recoil_glock18
+    {
+        kickBack[] = {0.04,0.08};
+        muzzleOuter[] = {0.05,0.45,0.2,0.2};
+        permanent = 0.01;
+        temporary = 0.005;
+    };
 };
