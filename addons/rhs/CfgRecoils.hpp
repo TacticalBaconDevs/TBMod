a74ca75a554e639416091b99c251d7a1d38422b0
E-for-Eron <17111@gmx.de>
2021-01-16 03:28:00 +0100

    G17 wieder raus, war nur als Vergleich gedacht

--- addons/rhs/CfgRecoils.hpp
+++ addons/rhs/CfgRecoils.hpp
@@ -4,13 +4,6 @@
 */
 class CfgRecoils
 {
-    class TB_recoil_glock17
-    {
-        kickBack[] = {"0.03*1.4","0.06*1.4"};
-        muzzleOuter[] = {"0.2*0.2","1*1","0.2*1","0.3*1"};
-        permanent = "0.1*0.4";
-        temporary = "0.03*1.2";
-    };
     class TB_recoil_glock18
     {
         kickBack[] = {0.04,0.08};
