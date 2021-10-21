cbb7ab7f379a31fe8eba39a9477769d330c86a82
shukari <shukari@users.noreply.github.com>
2021-10-21 23:06:51 +0200

    Revert "Revert "aus #613 neuen cmd connectToServer genutzt; script_mod.hpp und script_macros.hpp con common nutzen""
    This reverts commit 514bfde7fe6d88e554d141d65ac74ac51002e819.

--- addons/spectrum/script_component.hpp
+++ addons/spectrum/script_component.hpp
@@ -3,10 +3,10 @@
     Developed by http://tacticalbacon.de
 */
 #define COMPONENT spectrum
-#include "\x\TBMod\addons\main\script_mod.hpp"
+#include "\x\TBMod\addons\common\script_mod.hpp"
 
 // #define DEBUG_MODE_FULL
 // #define DISABLE_COMPILE_CACHE
 // #define ENABLE_PERFORMANCE_COUNTERS
 
-#include "\x\TBMod\addons\main\script_macros.hpp"
+#include "\x\TBMod\addons\common\script_macros.hpp"
